7135904b547c30edc487c78a1cbd93865337d19c
Denis Senkov <defendercryak@gmail.com>
2022-10-16 21:57:05 +0300

    Initial commit

---
+++ src/Pract_18_1/Main.java
@@ -0,0 +1,32 @@
+package Pract_18_1;
+public class Main {
+    public static void main(String[] args)
+    {
+      /**  ThrowsDemo c = new ThrowsDemo();
+       try
+       {
+           c.printMessage(null);
+       }
+       catch (Exception e)
+       {
+           e.printStackTrace();
+       };**/
+
+      ThrowsDemo c = new ThrowsDemo();
+        System.out.println("Введите ключ");
+      c.getKey();
+
+
+       Exception1 a=new Exception1();
+       a.exceptionDemo();
+
+        Exception2 b= new Exception2();
+        b.exceptionDemo();
+
+
+
+
+
+
+    }
+}
